fc403b13072dd4ed73a5ed4f965ce7ddc02ba025
PitchRE <hi@pitchblack.eu>
2020-03-02 01:00:12 +0100

    gitignore test

--- module_info.py
+++ module_info.py
@@ -1,5 +1,5 @@
 
-# Point export_dir to the folder you will be keeping your module
+# Point export_dir to the folder you will be keeping your module.
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
 #export_dir = "c:/MiB-OiM/MiB-New World/MiB-New World/Build/Modules/Ogniem i Mieczem/"
